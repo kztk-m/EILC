2cd3b46ee007ba59f772691205422ae35c5fd7aa
Kazutaka Matsuda <kztk@ecei.tohoku.ac.jp>
2021-11-26 18:16:37 +0900

    Clean up

--- src/Data/Delta.hs
+++ src/Data/Delta.hs
@@ -63,15 +63,7 @@ class Monoid (Delta a) => Diff a where
   checkEmpty = foldrDelta (\_ _ -> False) True
   {-# INLINABLE checkEmpty #-}
 
-  -- -- Especially for functions, empty update is not unique and cannot be obtained of the air.
-  -- emptify :: Delta a -> Delta a
-  -- default emptify :: Monoid (Delta a) => Delta a -> Delta a
-  -- emptify = const mempty
-
-  -- emptyOf :: a -> Delta a
-  -- default emptyOf :: Monoid (Delta a) => a -> Delta a
-  -- emptyOf = const mempty
-
+-- | A type-restricted version of @const mempty@.
 nilChangeOf :: Monoid (Delta a) => a -> Delta a
 nilChangeOf _ = mempty
 
@@ -84,6 +76,7 @@ foldl'Delta :: HasAtomicDelta a => (b -> AtomicDelta a -> b) -> b -> Delta a -> 
 foldl'Delta f z0 xs = foldrDelta f' id xs z0
       where f' x k z = k $! f z x
 {-# INLINE foldl'Delta #-}
+
 class (Monoid (Delta a), Diff a) => HasAtomicDelta a where
   data family AtomicDelta a :: Type
 
@@ -123,22 +116,24 @@ deriving instance (Show (Delta a), Show (Delta b)) => Show (Delta (a, b))
 instance Semigroup (Delta ()) where
   _ <> _ = UnitDelta
   {-# INLINE (<>) #-}
+
 instance Monoid (Delta ())    where
   mempty = UnitDelta
   {-# INLINE mempty #-}
+
 instance Diff () where
   a /+ _ = a
   {-# INLINE (/+) #-}
+
   checkEmpty _ = True
+  {-# INLINE checkEmpty #-}
 
 instance (Diff a, Diff b) => Diff (a, b) where
   (a, b) /+ PairDelta da db = (a /+ da, b /+ db)
   {-# INLINE (/+) #-}
 
   checkEmpty (PairDelta da db) = checkEmpty da && checkEmpty db
-
-  -- emptify (PairDelta da db) = PairDelta (emptify da) (emptify db)
-  -- emptyOf (a, b) = PairDelta (emptyOf a) (emptyOf b)
+  {-# INLINE checkEmpty #-}
 
 instance (Semigroup (Delta a), Semigroup (Delta b)) => Semigroup (Delta (a, b)) where
   PairDelta da1 db1 <> PairDelta da2 db2 = PairDelta (da1 <> da2) (db1 <> db2)
@@ -146,16 +141,20 @@ instance (Semigroup (Delta a), Semigroup (Delta b)) => Semigroup (Delta (a, b)) 
 
 instance (Monoid (Delta a), Monoid (Delta b)) => Monoid (Delta (a, b)) where
   mempty = PairDelta mempty mempty
+  {-# INLINE mempty #-}
 
 -- For compatibility
 pairDelta :: Delta a -> Delta b -> Delta (a, b)
 pairDelta = PairDelta
+{-# INLINE pairDelta #-}
 
 fstDelta :: Delta (a, b) -> Delta a
 fstDelta (PairDelta da _) = da
+{-# INLINE fstDelta #-}
 
 sndDelta :: Delta (a, b) -> Delta b
 sndDelta (PairDelta _ db) = db
+{-# INLINE sndDelta #-}
 
 instance (HasAtomicDelta a, HasAtomicDelta b) => HasAtomicDelta (a, b) where
   data AtomicDelta (a, b) = ADFst (AtomicDelta a) | ADSnd (AtomicDelta b)
@@ -175,12 +174,12 @@ instance Monoid (Delta a) => Monoid (Delta (Identity a)) where
   mempty = IdentityDelta mempty
 
 instance Diff a => Diff (Identity a) where
-  Identity a /+ (IdentityDelta da) = Identity (a /+ da)
+  (/+) = coerce ((/+) :: a -> Delta a -> a)
+  {-# INLINE (/+) #-}
 
-  checkEmpty (IdentityDelta da) = checkEmpty da
+  checkEmpty = coerce (checkEmpty :: Delta a -> Bool)
+  {-# INLINE checkEmpty #-}
 
-  -- emptify = coerce (emptify :: Delta a -> Delta a)
-  -- emptyOf = coerce (emptyOf :: a -> Delta a)
 
 instance HasAtomicDelta a => HasAtomicDelta (Identity a) where
   newtype instance AtomicDelta (Identity a) = ADIdentity { runADIdentity :: AtomicDelta a }
@@ -226,133 +225,6 @@ instance Diff Double where
   checkEmpty (DDouble n) = n == 0
   {-# INLINE checkEmpty #-}
 
-
-
 class (Diff a, Typeable a) => DiffTypeable a
 instance (Diff a, Typeable a) => DiffTypeable a
 
--- class (Applicative m, Alternative m, Foldable m) => MonoidF m
-
--- injMonoid :: MonoidF m => a -> m a
--- injMonoid = pure -- compatibility
-
--- monoidMap :: (MonoidF m, Monoid n) => (a -> n) -> m a -> n
--- monoidMap = foldMap
-
--- monoidFromList :: MonoidF m => [a] -> m a
--- monoidFromList = foldr ((<|>) . injMonoid) empty
-
--- -- Expecting foldr/build
--- {-# INLINE monoidFromList #-}
-
-
--- -- mmap :: (MonoidF a m, MonoidF b n) => (a -> b) -> m -> n
--- -- mmap f = monoidMap (injMonoid . f)
-
--- instance MonoidF []
-
--- newtype FreeMonoidF a =
---     FreeMonoidF { foldFreeMonoidF :: forall n. Monoid n => (a -> n) -> n }
-
--- instance Show a => Show (FreeMonoidF a) where
---   showsPrec k xs = showParen (k > 9) s
---     where
---       s = Text.Show.showString "monoidFromList" . showChar ' ' . shows (foldFreeMonoidF xs $ \x -> [x])
-
-
--- instance Functor FreeMonoidF where
---   fmap f xs = FreeMonoidF $ \h -> foldFreeMonoidF xs (h . f)
---   {-# INLINE fmap #-}
-
--- instance Semigroup (FreeMonoidF a) where
---   xs <> ys = FreeMonoidF $ \h -> foldFreeMonoidF xs h <> foldFreeMonoidF ys h
---   {-# INLINE (<>) #-}
-
--- instance Monoid (FreeMonoidF a) where
---   mempty = FreeMonoidF $ const mempty
---   {-# INLINE mempty #-}
-
--- instance Applicative FreeMonoidF where
---   pure a = FreeMonoidF $ \h -> h a
---   {-# INLINE pure #-}
---   f <*> x = foldFreeMonoidF f $ \f' -> foldFreeMonoidF x $ \x' -> pure (f' x')
---   {-# INLINE (<*>) #-}
-
--- instance Alternative FreeMonoidF where
---   empty = mempty
---   {-# INLINE empty #-}
-
---   (<|>) = (<>)
---   {-# INLINE (<|>) #-}
-
--- instance Foldable FreeMonoidF where
---   foldMap h as = foldFreeMonoidF as h
---   {-# INLINE foldMap #-}
-
--- instance MonoidF FreeMonoidF
-
--- -- Non-injectivity is usually a source of a trouble.
--- data family AtomicDelta (a :: Type) :: Type
-
--- newtype instance AtomicDelta (Identity a) = ADIdentity { runADIdentity :: AtomicDelta a }
--- data instance AtomicDelta ()
--- data instance AtomicDelta (a, b) = ADFst (AtomicDelta a) | ADSnd (AtomicDelta b)
-
--- deriving instance (Show (AtomicDelta a), Show (AtomicDelta b)) => Show (AtomicDelta (a, b))
-
--- fstDelta :: Delta (a, b) -> Delta a
--- fstDelta = foldMap fstDeltaA
-
--- fstDeltaA :: AtomicDelta (a, b) -> Delta a
--- fstDeltaA (ADFst da) = injMonoid da
--- fstDeltaA _          = mempty
-
--- sndDelta :: Delta (a, b) -> Delta b
--- sndDelta = foldMap sndDeltaA
-
--- sndDeltaA :: AtomicDelta (a, b) -> Delta b
--- sndDeltaA (ADSnd db) = injMonoid db
--- sndDeltaA _          = mempty
-
--- pairDelta :: Delta a -> Delta b -> Delta (a, b)
--- pairDelta xs ys = fmap ADFst xs <> fmap ADSnd ys
-
--- class Diff a where
---   applyAtomicDelta :: a -> AtomicDelta a -> a
-
---   -- | Applying delta.
---   -- prop> a /+ da /+ da' = a /+ (da <> da')
---   (/+) :: a -> Delta a -> a
---   a /+ das =
---     -- appEndo (getDual (monoidMap (\da -> Dual $ Endo $ \a -> applyAtomicDelta a da) das)) a
---     foldl applyAtomicDelta a das
-
--- instance Diff () where
---   applyAtomicDelta () _ = ()
-
--- instance (Diff a, Diff b) => Diff (a, b) where
---   -- (a, b) /+ (da, db) = (a /+ da, b /+ db)
---   applyAtomicDelta (a, b) (ADFst da) = (applyAtomicDelta a da, b)
---   applyAtomicDelta (a, b) (ADSnd db) = (a, applyAtomicDelta b db)
-
--- type Delta a = FreeMonoidF (AtomicDelta a)
-
-
--- -- State-Writer, both are composed left-to-right
--- newtype StateWriterLL s w = StateWriterLL { unStateWriterLL :: s -> (w, s) }
-
--- instance Semigroup w => Semigroup (StateWriterLL s w) where
---   f <> g = StateWriterLL $ \s ->
---     let (m1, s1) = unStateWriterLL f s
---         (m2, s2) = unStateWriterLL g s1
---     in (m1 <> m2, s2)
---   {-# INLINABLE (<>) #-}
-
--- instance Monoid w => Monoid (StateWriterLL s w) where
---   mempty = StateWriterLL $ \s -> (mempty , s)
-
--- iterTrStateless :: (AtomicDelta a -> Delta b) -> (Delta a -> Delta b)
--- iterTrStateless = foldMap
-
--- iterTr :: (AtomicDelta a -> c -> (Delta b, c)) -> (Delta a -> c -> (Delta b, c))
--- iterTr f = unStateWriterLL . monoidMap (StateWriterLL . f)
